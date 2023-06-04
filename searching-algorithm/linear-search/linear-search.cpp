a89578b0beb9e5025918f232bfd5bc7cd0d609a9
ikysantoso <ikysantoso1@gmail.com>
2023-06-04 14:05:32 +0700

    linear search update

--- searching-algorithm/linear-search/linear-search.cpp
+++ searching-algorithm/linear-search/linear-search.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+int search(int arr[], int N, int x)
+{
+    for (int i = 0; i < N; i++)
+        if (arr[i] == x)
+            return i;
+            
+    return -1;
+}
+
 int main()
 {
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int x = 10;
+    int N = sizeof(arr) / sizeof(arr[0]);
+
+    int result = search(arr, N, x);
+    (result == -1)
+        ? std::cout << "Element is not present in array"
+        : std::cout << "Element is present at index " << result;
+
     return 0;
 }
