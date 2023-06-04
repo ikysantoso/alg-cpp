9f28707fb5c61e54e70b8129b63f8bc19bd3be92
ikysantoso <ikysantoso1@gmail.com>
2023-06-04 14:24:48 +0700

    binary search ini t

--- searching-algorithm/binary-search/binary-search.cpp
+++ searching-algorithm/binary-search/binary-search.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
+int search(int arr[], int l, int r, int x)
+{
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+ 
+        if (arr[m] == x)
+            return m;
+ 
+        if (arr[m] < x)
+            l = m + 1;
+ 
+        else
+            r = m - 1;
+    }
+ 
+    return -1;
+}
+
 int main()
 {
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int x = 10;
+    int N = sizeof(arr) / sizeof(arr[0]);
+
+    int result = search(arr, 0, N - 1, x);
+    (result == -1)
+        ? std::cout << "Element is not present in array"
+        : std::cout << "Element is present at index " << result;
+
     return 0;
 }
